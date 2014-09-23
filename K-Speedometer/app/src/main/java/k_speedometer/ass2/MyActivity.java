efde6150b5574624ddd2c7a9c44ed0b752b130f6
Ditjon Berisha <ditjon.berisha@hig.no>
2014-09-23 18:41:45 +0200

    version 1

---
+++ K-Speedometer/app/src/main/java/k_speedometer/ass2/MyActivity.java
@@ -0,0 +1,74 @@
+package k_speedometer.ass2;
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MyActivity extends Activity implements LocationListener {
+
+    Speedometer speedometer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        speedometer = (Speedometer) findViewById(R.id.Speedometer);
+        LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        this.onLocationChanged(null);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(location == null){
+            speedometer.onSpeedChanged(0);
+        }
+        else {
+            float currentspeed = location.getSpeed() * 36/10;
+
+            speedometer.onSpeedChanged(currentspeed);
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
---
+++ K-Speedometer/app/src/main/java/k_speedometer/ass2/SpeedChangeListener.java
@@ -0,0 +1,8 @@
+package k_speedometer.ass2;
+
+/**
+ * Created by Ditjon on 9/23/2014.
+ */
+public interface SpeedChangeListener {
+    public void onSpeedChanged(float newSpeedValue);
+}
